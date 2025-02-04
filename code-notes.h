2cc864b0dd14c8916f5fa23d01d56a44386bf3c9
jomer ツ <mojer3102@gmail.com>
2025-02-04 10:23:30 -0800

    Create code-notes.h

---
+++ code-notes.h
@@ -0,0 +1,9 @@
+class myClass {
+  public:
+    myClass();
+    myClass(string name, const int age, string gender);
+  private:
+    string firstVariable;
+    int secondVariable;
+    string thirdVariable;
+}
