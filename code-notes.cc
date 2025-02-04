daa7e313551e9b2d3f42dff521b8bc7217a3fd45
jomer ツ <mojer3102@gmail.com>
2025-02-04 10:21:50 -0800

    Create code-notes.cc

---
+++ code-notes.cc
@@ -0,0 +1,4 @@
+#include "code-notes.h"
+
+myClass::myClass() : firstVaraible(""), secondVaraible(0), thirdVariable("") {}
+myClass::myClass(string name, const int age, string gender) : firstVariable(name), secondVariable(age), thirdVariable(gender) {}
