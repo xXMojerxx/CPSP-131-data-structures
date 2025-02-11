27145559c549e04a44a640b44665ab06e3163b7c
jomer ツ <mojer3102@gmail.com>
2025-02-11 10:17:26 -0800

    Update code-notes-main.cc

--- code-notes-main.cc
+++ code-notes-main.cc
@@ -6,7 +6,7 @@
 #include <code-notes.h>
 
 int main() {
-  std::string stringValue = "this is a string"
+  std::string stringValue = "this is a string";
 
   std::cout << std::ws << stringvalue; // std::ws represents "whits space" (ex: " ")
 }
