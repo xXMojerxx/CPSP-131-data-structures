69bce030ddb9e18d56f8c213ed4bb65a7499c68c
jomer ツ <mojer3102@gmail.com>
2025-02-11 10:30:12 -0800

    Update code-notes.cc

--- code-notes.cc
+++ code-notes.cc
@@ -1,4 +1,16 @@
 #include "code-notes.h"
 
-myClass::myClass() : firstVaraible(""), secondVaraible(0), thirdVariable("") {}
-myClass::myClass(string name, const int age, string gender) : firstVariable(name), secondVariable(age), thirdVariable(gender) {}
+myClass::myClass() : _name("Jude"), _age(19), _gender("male") {}
+myClass::myClass(string name, const int age, string gender) : _name(name), _age(age), _gender(gender) {}
+
+std::string myClass::getName() {
+  return _name;
+}
+
+int myClass::getAge() {
+  return _age;
+}
+
+std::string myClass::getGender() {
+  return _gender;
+}
