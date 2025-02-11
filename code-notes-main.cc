4060f69fba492c5359e34bba6f577de6432e11d3
jomer ツ <mojer3102@gmail.com>
2025-02-11 10:30:18 -0800

    Update code-notes-main.cc

--- code-notes-main.cc
+++ code-notes-main.cc
@@ -7,6 +7,17 @@
 
 int main() {
   std::string stringValue = "this is a string";
+  std::double input;
 
-  std::cout << std::ws << stringvalue; // std::ws represents "whits space" (ex: " ")
+  std::cout << std::ws << stringvalue;         // std::ws represents "whits space" (ex: " ")
+                                               // std::cout represents an output to terminal
+  std::cin >> input;                           // std::cin represtns and input from terminal -> sets it to variable presented
+
+  if (input == stringValue)                    // boolean check in "if" statement
+  {
+    cout << "this is the same";
+  }
+
+  myClass person{};                            // calling a class called "person"
+  
 }
