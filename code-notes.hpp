f0a34685eaf12bd835a9124a97e3033d601bcb0d
jomer ツ <mojer3102@gmail.com>
2025-02-28 21:09:54 -0800

    Update code-notes.hpp

--- code-notes.hpp
+++ code-notes.hpp
@@ -1,9 +1,29 @@
+#include <algorithm>                                                      // move( range ), move_backward( range )
+#include <array>
+#include <cstddef>                                                        // size_t
+#include <exception>
+#include <forward_list>
+#include <iomanip>                                                        // setprecision()
+#include <iostream>                                                       // boolalpha(), showpoint(), fixed(), setw()
+#include <list>
+#include <sstream>                                                        // ostringstream
+#include <string>                                                         // string, to_string()
+#include <utility>                                                        // move( object )
+#include <vector>
+
 class myClass {
   public:
     myClass();
     myClass(string name, const int age, string gender);
+
+
   private:
     string _name;
     int _age;
     string _gender;
+
+    // for singly linked list:
+    struct Node;
+    struct PrivateMembers;
+
 }
