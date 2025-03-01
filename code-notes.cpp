19c4852e8668f58d263224493f3352959e9c91f6
jomer ツ <mojer3102@gmail.com>
2025-02-28 21:08:23 -0800

    Update code-notes.cpp

--- code-notes.cpp
+++ code-notes.cpp
@@ -14,3 +14,23 @@ int myClass::getAge() {
 std::string myClass::getGender() {
   return _gender;
 }
+
+template<typename T>
+struct SinglyLinkedList<T>::Node
+{
+ Node() = default;
+ Node( T value ) : _data{ std::move(value) ) {}
+
+T _data = T{};
+Node * _next = nullptr;
+};
+// This is the definition of a singular linked list null-terminated
+
+template<typename t>
+struct SinglyLinkedList<T>::PrivateMembers
+{
+ Node * _head = nullptr;
+ Node * _tail = nullptr;
+ std::size_t _size = 0;
+};
+// This is the default construction of a singly linked list null-terminated.
